dec07997613ddd0d73020a7c84976a60afd000a3
Kamil Bronowski <kbronowski95@gmail.com>
2023-03-05 22:20:34 +0100

    Code refactoring

--- src/components/filter-chip.ts
+++ src/components/filter-chip.ts
@@ -1,37 +1,35 @@
-// import { Component } from "../models/component";
+import { Component } from "../models/component.js";
+import { FilterModel } from "../models/filter.js";
 
-// export class FilterChip extends Component<HTMLDivElement, HTMLDivElement> {
-//     private item: FilterModel;
-//     private itemName: HTMLDivElement;
-//     private itemIconSelected: HTMLSpanElement;
-//     private onSelect: () => void;
+export class FilterChip extends Component<HTMLDivElement, HTMLDivElement> {
+    private item: FilterModel;
+    private itemName: HTMLDivElement;
+    private itemIconSelected: HTMLSpanElement;
+    private onSelect: (selectedItem: FilterModel) => void;
 
-//     constructor(hostId: string, item: FilterModel, onSelect: () => void) {
-//         super("filter-item-template", hostId, false, "filter-chip-root");
-//         this.itemName = this.element.querySelector("#filter-item-name")! as HTMLDivElement;
-//         this.itemIconSelected = this.element.querySelector("span")! as HTMLSpanElement;
-//         this.onSelect = onSelect;
-//         this.item = item;
-//         this.renderContent();
-//     }
+    constructor(hostId: string, item: FilterModel, onSelect: (item: FilterModel) => void) {
+        super("filter-item-template", hostId, false, "filter-chip-root");
+        this.itemName = this.element.querySelector("#filter-item-name")! as HTMLDivElement;
+        this.itemIconSelected = this.element.querySelector("span")! as HTMLSpanElement;
+        this.onSelect = onSelect;
+        this.item = item;
+        this.renderContent();
+    }
 
-//     configure(): void {
-//     }
+    configure(): void {}
 
-//     renderContent(): void {
-//         this.setupComponents();
+    renderContent(): void {
+        this.setupComponents();
 
-//         this.element.addEventListener("click", (e) => {
-//             var index = filterItems.indexOf(this.item);
-//             filterItems[index].isSelected = !filterItems[index].isSelected;
-//             this.setupComponents();
-//             this.onSelect();
-//         });
-//     }
+        this.element.addEventListener("click", (e) => {
+            this.onSelect(this.item);
+            this.setupComponents();
+        });
+    }
 
-//     private setupComponents() {
-//         this.itemIconSelected.style.display = this.item.isSelected ? 'inline' : 'none';
-//         this.element.className = this.item.isSelected ? 'filter-item selected' : 'filter-item';
-//         this.itemName.textContent = this.item.name;
-//     } 
-// } 
+    private setupComponents() {
+        this.itemIconSelected.style.display = this.item.isSelected ? 'inline' : 'none';
+        this.element.className = this.item.isSelected ? 'filter-item selected' : 'filter-item';
+        this.itemName.textContent = this.item.name;
+    } 
+} 
--- src/components/filter.ts
+++ src/components/filter.ts
@@ -1,6 +1,7 @@
 import { Component } from "../models/component.js";
 import { FilterModel } from "../models/filter.js";
 import { foodAllergensTypes } from "../data/allergen-types.js";
+import { FilterChip } from "./filter-chip.js";
 
 export const filterItems = foodAllergensTypes.map((allergen) => { return new FilterModel(allergen.code, allergen.name, false); });
 
@@ -25,37 +26,4 @@ export class Filter extends Component<HTMLDivElement, HTMLDivElement> {
             });
         });
     }
-} 
-
-export class FilterChip extends Component<HTMLDivElement, HTMLDivElement> {
-    private item: FilterModel;
-    private itemName: HTMLDivElement;
-    private itemIconSelected: HTMLSpanElement;
-    private onSelect: (selectedItem: FilterModel) => void;
-
-    constructor(hostId: string, item: FilterModel, onSelect: (item: FilterModel) => void) {
-        super("filter-item-template", hostId, false, "filter-chip-root");
-        this.itemName = this.element.querySelector("#filter-item-name")! as HTMLDivElement;
-        this.itemIconSelected = this.element.querySelector("span")! as HTMLSpanElement;
-        this.onSelect = onSelect;
-        this.item = item;
-        this.renderContent();
-    }
-
-    configure(): void {}
-
-    renderContent(): void {
-        this.setupComponents();
-
-        this.element.addEventListener("click", (e) => {
-            this.onSelect(this.item);
-            this.setupComponents();
-        });
-    }
-
-    private setupComponents() {
-        this.itemIconSelected.style.display = this.item.isSelected ? 'inline' : 'none';
-        this.element.className = this.item.isSelected ? 'filter-item selected' : 'filter-item';
-        this.itemName.textContent = this.item.name;
-    } 
 } 
